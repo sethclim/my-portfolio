4a03d263cb6f81ac0863df23ae030dc557806ecc
sethclim <56656904+sethclim@users.noreply.github.com>
2023-12-26 19:39:36 -0500

    added loading bio + map work bullets

--- src/components/LayoutComponents.tsx
+++ src/components/LayoutComponents.tsx
@@ -1,4 +1,4 @@
-export const VStack = ({children, alignItems, justifyContent, h, bg, p, id, w, m, className}) =>{
+export const VStack = ({children, alignItems, justifyContent, h, bg, p, id, w, m, className} : any) =>{
     return (
         <div id={id} className={className} style={{ display: "flex", flexDirection: "column", alignItems:alignItems, justifyContent:justifyContent, height:h, width: w, backgroundColor:bg, padding: p, margin: m }}>
             {children}
@@ -6,7 +6,7 @@ export const VStack = ({children, alignItems, justifyContent, h, bg, p, id, w, m
     )
 }
 
-export const HStack = ({children, alignItems, justifyContent, h, bg, p, m, w, className}) =>{
+export const HStack = ({children, alignItems, justifyContent, h, bg, p, m, w, className} : any) =>{
     return (
         <div className={className} style={{ display: "flex", flexDirection: "row", alignItems:alignItems, justifyContent:justifyContent, height:h, width:w , padding: p, margin: m, backgroundColor:bg }}>
             {children}
@@ -15,7 +15,7 @@ export const HStack = ({children, alignItems, justifyContent, h, bg, p, m, w, cl
 }
 
 
-export const Grid = ({children, templateColumns, templateRows, gap, id}) =>{
+export const Grid = ({children, templateColumns, templateRows, gap, id} : any) =>{
     return (
         <div id={id} style={{ display: "grid", gridTemplateColumns: templateColumns, gridTemplateRows: templateRows, gap:gap, width:"100%"}}>
             {children}
@@ -23,11 +23,11 @@ export const Grid = ({children, templateColumns, templateRows, gap, id}) =>{
     )
 }
 
-export const GridItem = ({children, w, h, bg, onClick, onMouseLeave, onMouseOver, justifyContent, alignItems, className, p}) =>{
+export const GridItem = ({children, w, h, bg, onClick, onMouseLeave, onMouseOver, justifyContent, alignItems, className, p} : any) =>{
     return (
         <div 
             className={className}
-            style={{ display: "flex", flexDirection: "column", alignItems:alignItems, justifyContent:justifyContent, height:h, width:w, backgroundColor: bg, p:p}} 
+            style={{ display: "flex", flexDirection: "column", alignItems:alignItems, justifyContent:justifyContent, height:h, width:w, backgroundColor: bg, padding:p}} 
             onMouseLeave={onMouseLeave}
             onMouseOver={onMouseOver}
             onClick={onClick}
--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,68 +1,73 @@
-import React from "react";
-
 import { HStack, VStack, Grid, GridItem } from "../../LayoutComponents";
 import Copy from "../../Copy";
 import Text from "../../../styles/text.module.scss"
 import Styles from "./About.module.scss"
 
-class About extends React.Component {
-  render() {
-    return (
+export type AboutProps ={
+  bio : any
+}
+
+const About = (props : AboutProps) =>  {
+
+  return(
       <VStack id="about">
         <VStack p="1em 0">
           <p className={Text.Title}>About</p>
           {/* <hr className="new4"></hr> */}
-
-            <p className={Text.small}>
-            As a 4th year honours student in computer science at Sheridan College, I have honed my software development skills across many platforms including web, mobile, and desktop applications.  In addition, I have experience with data science and AI/machine learning applications     </p><br></br>
-            <p className={Text.small}>Recently, I  completed an internship as a VR developer where I led development on a simulated training module for LSTA. I worked with external advisers on the project and developed a complete solution based on the derived requirements.  I also worked as an Intern at TCXR, where I developed and improved a data pipe for a Unity project.  I was responsible for designing and implementing new components of the system, and interfacing with the data team to communicate changes and gather new requirements. </p><br></br>
-            <p className={Text.small}>I am passionate about building user-driven interfaces, robust backends, and creative branding that brings value to whatever system I’m developing. I possess a self-driven entrepreneurial mindset with a relentless determination to solve problems. </p><br></br>
-            <p className={Text.small}>I believe that my strong work ethic developed through my extensive work experiences and dedication to enhancing my skills through my studies make me an ideal employee. I’m currently looking for internships and work opportunities in software development.
-            </p>
+            <p className={Text.small}>{props.bio[0].frontmatter.bio}</p>
         </VStack>
-
         <VStack p="1em 0">
           <p className={Text.Title}>Work Experience</p>
           <Work
            company="SpeakSynk Technology" 
            role="Algorithm Developer" 
            date="May 2023 - Present" 
-           point1="Built initial product MVP to demonstrate to investor"
-           point2="Deployed scalable version of product to AWS"
-           point3="Improved ML models, and computer vision algorithms"
-           point4="Developed backend infrascture components to serve customers"
+           points={[
+            "Built initial product MVP to demonstrate to investor",
+            "Deployed scalable version of product to AWS",
+            "Improved ML models, and computer vision algorithms",
+            "Developed backend infrascture components to serve customers"
+           ]}
            />
           <Work
            company="Artful Enrichment" 
            role="Mobile Developer" 
            date="Sept. 2023 - Present" 
-           point1="Developed mobile app in React Native"
-           point2="Implemented intuitive UI for seniors"
-           point3="Interfaced with backend APIs for business logic"
+           points={[
+            "Developed mobile app in React Native",
+            "Implemented intuitive UI for seniors",
+            "Interfaced with backend APIs for business logic"
+           ]}
            />
           <Work
            company="Sheridan Center For Mobile Innovation" 
            role="VR Developer" 
            date="May 2022 - May 2023" 
-           point1="Developed VR interaction to simulate pharmaceutical factory"
-           point2="3D modeled pharmaceutical factory equipment"
-           point3="Designed simulation software architecture"
+           points={[
+            "Developed VR interaction to simulate pharmaceutical factory",
+            "3D modeled pharmaceutical factory equipment",
+            "Designed simulation software architecture"
+           ]}
            />
           <Work
            company="The Commons XR" 
            role="Unity Developer" 
            date="April 2022 - Nov. 2022" 
-           point3="Designed custom solutions for data collection from Unity"
-           point1="Implemented data collection solutions"
-           point2="Interfaced Between Data and Unity Team"
+           points={[
+            "Implemented data collection solutions",
+            "Interfaced Between Data and Unity Team",
+            "Designed custom solutions for data collection from Unity"
+           ]}
            />
           <Work
            company="Sample House" 
            role="Full Stack Developer" 
            date="Aug 2021 - Dec. 2021" 
-           point3="Redesigned frontend written in React to a modern clean design to present to investors."
-           point1="Implemented waveform player to preview audio samples and tooling to generate the waveform data."
-           point2="Worked on payment system and analytics integrations."
+           points={[
+            "Implemented waveform player to preview audio samples and tooling to generate the waveform data.",
+            "Worked on payment system and analytics integrations.",
+            "Redesigned frontend written in React to a modern clean design to present to investors."
+           ]}
            />
         </VStack>
 
@@ -106,28 +111,38 @@ class About extends React.Component {
       
       </VStack>
     );
-  }
 }
-export default About;
 
+export type WorkProps =  {
+  company : string,
+  role : string,
+  date : string,
+  points: Array<string>
+}
 
-const Work = ({company, role, date, point1, point2, point3 }) => {
+const Work = (props : WorkProps) => {
   return(
     <VStack w="100%" p="20px 0 0 0">
       <VStack className={Styles.workTitleWrap} p="0" h="100%">
         <VStack className={Styles.workWrap}  w="100%" p="0" m="0" justifyContent="flex-start">
-            <p className={Styles.name}>{role}</p>
-            <p className={Styles.cn}>{company}</p> 
+            <p className={Styles.name}>{props.role}</p>
+            <p className={Styles.cn}>{props.company}</p> 
         </VStack>
         <HStack className={Styles.dateWrap} w="100%" justifyContent="flex-start">
-          <p className={Styles.cn}>{date}</p>
+          <p className={Styles.cn}>{props.date}</p>
         </HStack>
       </VStack>
       <ul className={Styles.desc}>
-        <li>{point1}</li>
-        <li>{point2}</li>
-        <li>{point3}</li>
+        {
+          props.points.map((point, index) =>{
+            return (
+              <li key={index}>{point}</li>
+            )
+          })
+        }
       </ul>
     </VStack>
   )
 }
+
+export default About;
