c37190bee1bef078d880b64dbf02e7b06702e8f9
sethclim <sethclim@gmail.com>
2024-11-07 15:46:26 -0500

    try deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sethclim.github.io/',
+  base: 'my-portfolio',
   integrations: [react()]
 });
