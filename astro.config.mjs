552f348785d2ba8a172ffa10665a31cee9b695eb
sethclim <sethclim@gmail.com>
2025-04-05 20:41:00 -0400

    removed base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sethclimenhaga.dev',
-  base: 'my-portfolio',
   integrations: [react()],
 
   vite: {
